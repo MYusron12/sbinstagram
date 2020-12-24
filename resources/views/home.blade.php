6d6e07c1261cb79ac9d94156eb9d1a30c91b070d
myusron12 <ryusron19@gmail.com>
2020-12-24 09:11:24 +0700

    double klick like(masih eror)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,20 +10,27 @@
                 <div class="card-body">
                     <h3>Postingan</h3>
                     @foreach($posts as $post)
-                    <div class="mt-5">
-                        <img src="{{asset('images/posts/' . $post->image)}}" alt="{{$post->caption}}" width="200px" height="200px"/>
-                    </div>
-<div class="mt-3">
-                    {{-- link ke username / profile --}}
-                    <a href="/{{'@'.$post->user->username}}">{{'@'.$post->user->username}}</a>
-                    
-                    {{-- button like/unlike --}}
-                    <button class="btn btn-primary" onclick="like({{$post->id}}, this)">
-                        {{ ($post->is_liked() ? 'unlike' : 'like' ) }}
-                    </button>
-                </div>
+                    {{-- postingan --}}
+                    <div class="card mb-3" style="max-width: 18rem;">
+                        <div class="card-header">
+                            <a href="/{{'@'.$post->user->username}}">{{'@'.$post->user->username}}</a>
+                        </div>
+                        <div class="card-body">
+                                <img src="{{asset('images/posts/' . $post->image)}}" alt="{{$post->caption}}" width="200px" height="200px" ondblclick="like({{$post->id}})"/>
+                          <h5 class="card-title"></h5>
+                          <p class="card-text">Caption : 
+                              <strong>
+                                  {{$post->caption}}
+                            </strong> 
+                          </p>
+                          <button class="btn btn-primary" onclick="like({{$post->id}})" id="post-btn-{{$post->id}}">
+                              {{ ($post->is_liked() ? 'unlike' : 'like' ) }}
+                          </button>
+                        </div>
+                      </div>
                       <script>
-                          function like(id, el){
+                          function like(id){
+                              const el = document.getElementById('post-btn-' + id)
                               fetch('/like/' + id)
                               .then(Response => Response.json())
                               .then(data => {
